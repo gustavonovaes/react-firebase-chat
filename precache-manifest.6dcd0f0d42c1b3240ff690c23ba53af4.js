7e24005fbe44af3f09cc5e7722d44b1c438984ed
Gustavo Novaes <gustavonovaes93@gmail.com>
2020-10-28 11:24:26 -0300

    Updates

---
+++ precache-manifest.6dcd0f0d42c1b3240ff690c23ba53af4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "923105b1648ec93bdeefcb77c9f55235",
+    "url": "/react-firebase-chat/index.html"
+  },
+  {
+    "revision": "86c6b256cf4f955990e6",
+    "url": "/react-firebase-chat/static/css/main.85e24194.chunk.css"
+  },
+  {
+    "revision": "d07c7124788fb2507460",
+    "url": "/react-firebase-chat/static/js/2.6e67778f.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/react-firebase-chat/static/js/2.6e67778f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86c6b256cf4f955990e6",
+    "url": "/react-firebase-chat/static/js/main.675ef618.chunk.js"
+  },
+  {
+    "revision": "ba3c876433fc550ed02b",
+    "url": "/react-firebase-chat/static/js/runtime-main.6275de41.js"
+  }
+]);
